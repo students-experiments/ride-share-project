73c9ac742ca5b5db061cf213841be6b57a41bad4
Anoop <nanoop14@gmail.com>
2020-02-24 00:50:22 -0600

    Initial Commit

---
+++ test/login-test.js
@@ -0,0 +1,58 @@
+// Import the dependencies for testing
+const assert = require('assert');
+const axios = require('axios').default;
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
+const tough = require('tough-cookie');
+const stoppable = require('stoppable');
+
+const app = require('../app');
+
+axiosCookieJarSupport(axios);
+
+const PORT = 3000;
+
+describe('application', async () => {
+  /* fill these in before each test */
+  let server = {};
+  let client = {};
+
+  axios.defaults.withCredentials = true;
+  axios.defaults.baseURL = `http://localhost:${PORT}/`;
+  axios.defaults.validateStatus = () => true;
+
+  /* Utility functions
+   */
+  // Deterministic (for testing) Math.random replacement
+  // https://gist.github.com/mathiasbynens/5670917
+
+  const psrand = (() => {
+    let seed = 0xaabbccd;
+    return () => {
+      /* eslint-disable no-bitwise */
+      // Robert Jenkins’ 32 bit integer hash function
+      seed = (seed + 0x7ed55d16 + (seed << 12)) & 0xffffffff;
+      seed = (seed ^ 0xc761c23c ^ (seed >>> 19)) & 0xffffffff;
+      seed = (seed + 0x165667b1 + (seed << 5)) & 0xffffffff;
+      seed = ((seed + 0xd3a2646c) ^ (seed << 9)) & 0xffffffff;
+      seed = (seed + 0xfd7046c5 + (seed << 3)) & 0xffffffff;
+      seed = (seed ^ 0xb55a4f09 ^ (seed >>> 16)) & 0xffffffff;
+      return (seed & 0xfffffff) / 0x10000000;
+      /* eslint-enable no-bitwise */
+    };
+  })();
+
+  // https://gist.github.com/6174/6062387#gistcomment-2915959
+  function getRandomString(length) {
+    let s = '';
+    do {
+      s += psrand()
+        .toString(36)
+        .substr(2);
+    } while (s.length < length);
+    s = s.substr(0, length);
+    return s;
+  }
+
+  
+  
+});
