2e8484d6cd759e417d878dbd474b6a855c80c3a3
Anoop <nanoop14@gmail.com>
2020-02-29 20:21:40 -0600

    implemented routes in functions folder

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const engines = require('consolidate');
 
+
+const app=express();
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
+
+app.get('/',(request,response)=>{
+    response.render('index');
+})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.app = functions.https.onRequest(app);
