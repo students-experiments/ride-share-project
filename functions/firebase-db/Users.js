2e0e6cee003f0a3e16a9043619a650267b3394f9
Anoop <nanoop14@gmail.com>
2020-03-02 02:31:04 -0600

    included following: User registration add user's role User login will verify for the role

--- functions/firebase-db/Users.js
+++ functions/firebase-db/Users.js
@@ -1,16 +1,11 @@
 
 const firebase_admin = require("firebase-admin")
 const firebase=require("firebase");
+var path = require('path');
 
-const serviceAccount = require('/Users/anoopnagabhushan/Desktop/UIC/CS494/ride match/final-project-create-table-students/firebase-service-account.json');
-var refreshToken;
-const admin_config={
-    credentials:firebase_admin.credential.cert(serviceAccount),
-    databaseURL: "https://uic-rider.firebaseio.com"
-}
 
 const firebaseConfig = {
-    
+    credentials: firebase_admin.credential.applicationDefault(),
     apiKey: "AIzaSyAlfC_SV4Nbc9lfnmLnpOed58K9jYMB8N8",
     authDomain: "uic-rider.firebaseapp.com",
     databaseURL: "https://uic-rider.firebaseio.com",
@@ -20,28 +15,23 @@ const firebaseConfig = {
     appId: "1:158636708222:web:519350eb086a15306a632f",
     measurementId: "G-1QYZ38TLBN"
   };
-  
-  
-  // Initialize Firebase
-  firebase_admin.initializeApp({
-    credential: firebase_admin.credential.cert(serviceAccount),
-    databaseURL: "https://uic-rider.firebaseio.com"
-  });
-  firebase.initializeApp(firebaseConfig);
-  
 
-function writeUserToUserTable(params,role){
+  // Initialize Firebase
+  firebase_admin.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+
+  var db = firebase_admin.firestore();
+
+  function storeUserRole(username,role){
     return new Promise((resolve,reject)=>{
-        var db = firebase_admin.database();
-        console.log("uid in db fucn",params.uid);
-        var userref = db.ref('users/').child(params.uid);
-        console.log('path in DB',userref.key);
-        var data=prepareUserDBObject(params,role);
-        userref.set(data).then((result)=>{
-            data["path"]=userref.key;
+        var rolesRef = db.collection('roles').doc(role);
+        // Atomically add a new region to the "regions" array field.
+        let result=rolesRef.update({
+            registered : firebase_admin.firestore.FieldValue.arrayUnion(username)
+        }).then((result)=>{
+            data={"username":username,"role":role}
             resolve(data);
             return result;
-
         }).catch((error)=>{
             handleError(error);
             reject(new Error("DB call Failed. "+ error.message));
@@ -49,35 +39,84 @@ function writeUserToUserTable(params,role){
         })
     });
 }
+
+function storeUserData(params){
+    return new Promise((resolve,reject)=>{
+        
+        var userref = db.collection('users').doc(params.uid);
+        var data=prepareUserDBObject(params);
+        userref.set(data)
+        .then((result)=>{
+            data["path"]=userref.key;
+            resolve(params);
+            return result;
+
+        }).catch((error)=>{
+            handleError(error);
+            reject(new Error("DB call Failed. "+ error.message));
+            return ;
+        })
+    });
+}
+function validateUserRole(uid,role){
+
+    return new Promise((resolve,reject)=>{
+        console.log("invalidation",uid);
+        var userRef = db.collection('users').doc(uid);
+        userRef.get()
+        .then((doc)=> {
+            if (!doc.exists) {
+                reject(new Error("invalid uid"));
+            } 
+            if(doc.data().role===role){
+                console.log('valid user');
+                resolve('valid user');
+            }
+            else{
+                // eslint-disable-next-line prefer-promise-reject-errors
+                reject(new Error("Not right role"));
+            }
+            return
+        }).catch((error)=>{
+            console.log('User role validation Failed');
+            reject(new Error('User role validation Failed' + error.message));
+        });
+    });
+
+
+}
 function loginUser(user){
     console.log("user",user);
     return new Promise((resolve,reject)=>{
     firebase.auth().signInWithEmailAndPassword(user.email, user.pass)
-    .then(()=>{
-        console.log('User authenticated');
-        return resolve("success");
-    })
-    .catch((error) =>{
+    .then((data)=>{
+        console.log('User SignIn Successful');
+        let profile=firebase_admin.auth().getUserByEmail(user.email)
+        return profile;
+    }).then((profile)=>{
+        console.log(profile.uid)
+        let isUserRoleValid=validateUserRole(profile.uid,user.role)
+        return isUserRoleValid
+    }).then((isUserRoleValid)=>{
+        console.log("user status",isUserRoleValid);
+        resolve("success");
+        return
+    }).catch((error) =>{
         handleError(error);
         console.log('Login Failed.');
         reject(new Error("Login Failed. " + error.message));
       });
     });
 }
-function prepareUserDBObject(params,role){
+function prepareUserDBObject(user){
     var data;
-    if(role==='rider'){
-       data={
-            "role": params.role,
-            "username": params.username,
-            "uin": params.uin
-        };
-    }
-    else{
-        data={
-            "role": params.role,
-            "username": params.username,
-        };
+    data={
+        "role": user.role,
+        "username": user.username,
+        "email":user.email
+    };
+    if(user.role==='rider'){
+       data['uin']= user.uin
     }
     return data;
 
@@ -93,7 +132,7 @@ function handleError(error){
     console.log(errorMessage);
 }
 
-function validateUser(user,role){
+function validateUser(user){
     return new Promise((resolve,reject)=>{
         if(user.email && user.pass && user.username )
             resolve("valid user");
@@ -103,27 +142,31 @@ function validateUser(user,role){
     });
 }
 
-function registerUser(user,role){
+function registerUser(user){
 
     return new Promise((resolve,reject) =>{
         //first validate User
         console.log("user",user);
-        validateUser(user,role).then ((validatedUser)=>{
+        validateUser(user).then ((validatedUser)=>{
             console.log('user credentials verified',validatedUser);
             let auth=firebase.auth().createUserWithEmailAndPassword(user.email, user.pass);
             return auth;
-        }).then((result) => {
-            //User DB Call
-            console.log('user authenticated user:',user);
-            user.uid=result.user.uid;
-            userdbcall=writeUserToUserTable(user,role);
-            return userdbcall;
-        }).then((userdbcall) =>{
-            // Modify the DBCall Object before sending.
-            console.log('user Added to DB: ',userdbcall.uid);
+        }).then((auth)=>{
+            //store users data
+            console.log('user created with firebase:',user.username);
+            user['uid']=auth.user.uid;
+            let addUserToDb=storeUserData(user);
+            return addUserToDb;
+        }).then((addUserToDb) =>{
+            // Store Users role.
+            console.log('user Added to User DB: ',user.username);
+            let addUserToRole=storeUserRole(user.username,user.role);
+            return addUserToRole;
+        }).then((addUserToRole)=>{
+            console.log('user Added to Role DB: ',addUserToRole.username);
             return resolve('Success');
         }).catch((error)=>{
-            //console.log(error);
+            console.log(error);
             handleError(error);
             reject(new Error("Registration Failed. "+error.message));
         });
