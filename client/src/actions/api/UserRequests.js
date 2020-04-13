7e0c1e1791e8d15e365d0c9513cc77fbd29edda6
Anoop <nanoop14@gmail.com>
2020-04-13 18:57:16 -0500

    added register route in Login Page

--- client/src/actions/api/UserRequests.js
+++ client/src/actions/api/UserRequests.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
 
 export function postUserClaims(user,claims){
-
     const data={
         "claims":claims,
         "uid":user.uid
     }
+    const base_url = getBaseURL()
     const post_params={data}
     console.log('posting params',post_params);
-    return axios.post(`http://localhost:5001/uic-rider/us-central1/app/index/addUserClaims`, post_params);
+    const url=base_url+ 'index/addUserClaims' ;
+    return axios.post(url, post_params);
+}
+function getBaseURL() {
+    if(process.env.NODE_ENV === "development"){
+        console.log('dev env',process.env);
+        return process.env.REACT_APP_LOCAL_BACKEND_URL
+    }
+    console.log('prod env')
+    return process.env.REACT_APP_PROD_BACKEND_URL
 }
